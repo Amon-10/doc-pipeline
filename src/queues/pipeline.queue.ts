1d198ee2f29eb508cbab9bea113dc5ac6b3bc6f9
amanlal <alal09635@gmail.com>
2026-08-11 21:04:06 -0700

    fix: add password for redis for deployment on railway

--- src/queues/pipeline.queue.ts
+++ src/queues/pipeline.queue.ts
@@ -8,6 +8,7 @@ import { Queue } from "bullmq";
 export const connection = {
   host: process.env.REDIS_HOST || "redis",
   port: parseInt(process.env.REDIS_PORT || "6379"),
+  password: process.env.REDIS_PASSWORD || undefined,
 };
 
 /**
